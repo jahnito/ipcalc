005d74dee83098c407a98893747ab8c57f3dccbe
jahnito <jahnito@gmail.com>
2024-05-26 15:50:20 +0500

    d2

--- ipcalc.py
+++ ipcalc.py
@@ -3,16 +3,39 @@ from tkinter import *
 
 PRGNAME = 'IP Calc'
 PRGFONTTXT = ('TkFixedFont', 9)
+ERRINPUT = ''
 X,Y = 500,300
 DIM = f'{X}x{Y}'
 
 
-def set_addr():
-    addrT.configure(state='normal')
-    addrT.delete(1.0, END)
-    addrT.insert(1.0, '192.168.3.3')
-    addrT.configure(state='disabled')
+def ip_check(ip_input_str: str):
+    global ERRINPUT
+    try:
+        ipaddress.ip_interface(ip_input_str)
+        return True
+    except (ipaddress.AddressValueError, ValueError) as err:
+        ERRINPUT = err
+        return False
 
+
+def ip_solve(ip_int: ipaddress.ip_interface) -> dict:
+    print(ip_int.ip)
+    # print(ipaddress.ip_network(ip_int))
+
+
+def set_addr():
+    global ERRINPUT
+    ip_str = field.get()
+    if ip_check(ip_str):
+        ip_data = ip_solve(ipaddress.ip_interface(ip_str))
+        ERRINPUT = ''
+        errlabel.configure(text=ERRINPUT)
+    else:
+        errlabel.configure(text=ERRINPUT)
+    # addrT.configure(state='normal')
+    # addrT.delete(1.0, END)
+    # addrT.insert(1.0, '192.168.3.3')
+    # addrT.configure(state='disabled')
 
 
 if __name__ == '__main__':
@@ -23,6 +46,7 @@ if __name__ == '__main__':
     # Frames
     f1 = LabelFrame(padx=1, pady=1, text='Detail IP Data')
     f2 = Frame(padx=1, pady=1)
+    f3 = LabelFrame(padx=1, pady=1, text='More Info')
     f_addr = Frame(f1)
     f_netm = Frame(f1)
     f_wildc = Frame(f1)
@@ -41,6 +65,7 @@ if __name__ == '__main__':
     hostmx = Label(f_hostmx, text='HostMax:   ')
     bcast = Label(f_bcast, text='Broadcast: ')
     hosts = Label(f_hosts, text='Hosts:        ')
+    errlabel = Label(f2, font=('TkFixedFont', 7))
     #Text
     addrT = Text(f_addr, height=1, borderwidth=0, width=15, relief='flat', font=PRGFONTTXT)
     netmT = Text(f_netm, height=1, borderwidth=0, width=15, relief='flat', font=PRGFONTTXT)
@@ -57,6 +82,7 @@ if __name__ == '__main__':
 
     f1.pack(side='left', fill='both', anchor='nw', expand=True)
     f2.pack(side='right', fill='both', anchor='nw')
+    f3.pack(side='right', fill='both', anchor='nw')
     f_addr.pack(expand=True, anchor='nw')
     f_netm.pack(expand=True, anchor='nw')
     f_wildc.pack(expand=True, anchor='nw')
@@ -65,10 +91,13 @@ if __name__ == '__main__':
     f_hostmx.pack(expand=True, anchor='nw')
     f_bcast.pack(expand=True, anchor='nw')
     f_hosts.pack(expand=True, anchor='nw')
+    
 
     field.pack(side='top', fill='x')
+    field.focus_set()
 
     b1.pack(side='top')
+    errlabel.pack()
 
     # address
     addr.pack(side='left', anchor='w', )
--- ipcalc_console.py
+++ ipcalc_console.py
@@ -1,21 +1,22 @@
 import argparse
-
-IPM = ipaddress.ip_interface(args.ip_mask)
-IPN = IPM.network
+import ipaddress
 
 parser = argparse.ArgumentParser('description')
 parser.add_argument('-i', '--ip_mask', type=str, default='127.0.0.1/8', metavar='{ip/mask}', help='set ip addres with /mask, example: 192.168.88.1/24')
 args = parser.parse_args()
 
-# print(f'ip address: {IPM.ip}')
-# print(f'netmask: {IPM.netmask}')
-# print(f'network address: {IPN}')
-# print(f'min ip address: {IPN[1]}')
-# print(f'max ip address: {IPN[-2]}')
-# print(f'broadcast address: {IPN[-1]}')
-# print(f'hosts: {IPN.num_addresses}')
-# print()
-# print(f'private address: {IPM.is_private}')
-# print(f'global address: {IPM.is_global}')
-# print(f'loopback address: {IPM.is_loopback}')
-# print(f'multicast address: {IPM.is_multicast}')
+IPM = ipaddress.ip_interface(args.ip_mask)
+IPN = IPM.network
+
+print(f'ip address: {IPM.ip}')
+print(f'netmask: {IPM.netmask}')
+print(f'network address: {IPN}')
+print(f'min ip address: {IPN[1]}')
+print(f'max ip address: {IPN[-2]}')
+print(f'broadcast address: {IPN[-1]}')
+print(f'hosts: {IPN.num_addresses}')
+print()
+print(f'private address: {IPM.is_private}')
+print(f'global address: {IPM.is_global}')
+print(f'loopback address: {IPM.is_loopback}')
+print(f'multicast address: {IPM.is_multicast}')
